1165b468adab71ac3e44c4ed388c27bc98b9e1ff
emilia <emi1213galarza@gmail.com>
2023-11-02 22:26:10 -0500

    Modified Nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 function Nav({options}) {
   return (
-    <div>
-        <ul>
-            {options.map((opcion, index) => (
-                <li key={index}>
-                <a href={opcion.href}>{opcion.label}</a>
-                </li>
-            ))}
-        </ul>
+    <div className = 'bg-amber-950 '>
+      <ul className='flex justify-end items-center text-white'>
+        {options.map((opcion, index) => (
+          <li key={index} className='m-5'>
+            <a href={opcion.href}>{opcion.label}</a>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
